7fe8cca4e769fb459f8074bd7030d83fc588540a
Slavcho . Dimitrievski <slavcho.dimitrievski@MUSALA.COM>
2024-08-22 12:59:43 +0200

    Add footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,7 @@ function RootLayout({ children }) {
     <html lang="en">
       <body>
         {children}
-
+        <footer>Copyright Earth Universe Inc.</footer>
       </body>
     </html>
   );
